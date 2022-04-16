4e4b5a4aede9d1c99d24338382c61b8a56d13954
rafidmahin20 <submanmahin@gmail.com>
2022-04-16 21:29:27 +0600

    edited

--- illusion-wedding/src/App.js
+++ illusion-wedding/src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AboutMe from './Components/About Me/AboutMe';
+import Blogs from './Components/Blogs/Blogs';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Services from './Components/Services/Services';
 
 function App() {
   return (
     <div>
-      <h1 className='text-4xl text-red-600'>Hello there</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/aboutme' element={<AboutMe/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
