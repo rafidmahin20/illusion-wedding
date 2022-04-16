2dae8d3a7d7cbe630291063628ce10793ee12c4c
rafidmahin20 <submanmahin@gmail.com>
2022-04-16 21:16:50 +0600

    edited

---
+++ illusion-wedding/src/Components/CustomLink/CustomLink.js
@@ -0,0 +1,17 @@
+import React, { useRef } from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+const CustomLink = ({to, children, ...props}) => {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({path: resolved.pathname, end: true});
+    return (
+        <Link style={{color: match ? "yellow" : "Black"}}
+        to={to}
+        {...props}
+        >
+            {children}
+        </Link>
+    );
+};
+
+export default CustomLink;
