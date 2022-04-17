57f32559fa97efa7eed32cb0ad28f4a4c6103e04
rafidmahin20 <submanmahin@gmail.com>
2022-04-17 13:58:35 +0600

    edited

--- illusion-wedding/src/Components/Home/Home.js
+++ illusion-wedding/src/Components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                         </p>
                     </div>
                     <div className='flex aspect-w-3 aspect-h-2 order-1 md:order-2'>
-                        <img className='mx-auto object-cover h-90 w-90' src={weddingphoto1} alt=''></img>
+                        <img className='mx-auto object-cover cover-img' src={weddingphoto1} alt=''></img>
                     </div>
                 </div>
             </section>
@@ -32,7 +32,7 @@ const Home = () => {
             </section>
             <section >
                 <div className='section-fourth pb-30 mt-10'>
-                    <div className='flex justify-center pt-20 pb-20'>
+                    <div className='flex justify-center pt-60'>
                     <button type="button" class="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">Contact Us</button>
                     </div>
                 </div>
