4d8bb8210ae33440d3930cf70868e4ce2ca27e93
rafidmahin20 <submanmahin@gmail.com>
2022-04-17 13:48:48 +0600

    edited

--- illusion-wedding/src/Components/About Me/AboutMe.js
+++ illusion-wedding/src/Components/About Me/AboutMe.js
@@ -4,7 +4,7 @@ import me from '../../Images/me.jpg'
 const AboutMe = () => {
     return (
         <div className='px-4 pt-10 pb-10 mx-auto max-w-7xl md:px-24'>
-            <div className='h-full grid grid-cols-1 gap-20 md:grid-cols-2 gap-10'>
+            <div className='h-full grid grid-cols-1 gap-20 md:grid-cols-2'>
                 <div className='order-2 md:order-1'>
                     <p className='text-xl font-medium'>
                         - I want to be a front-end developer in future. Thats my dream that's why i joined this course and gain skill so that i can apply in different companies. In next 4/5 months i want to see myself doing an internship or job at any company. & in next 2/3 yrs i want to see myself leading a project for my client. That's my goal.
