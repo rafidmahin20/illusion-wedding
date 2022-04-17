208677681760e0f3791e9089b459ab35026054a6
rafidmahin20 <submanmahin@gmail.com>
2022-04-17 23:33:22 +0600

    edited

--- illusion-wedding/src/Components/Signup/SignUp.js
+++ illusion-wedding/src/Components/Signup/SignUp.js
@@ -14,22 +14,21 @@ const SignUp = () => {
             <form>
               <div className="grid grid-cols-2 gap-4">
                 <div className="form-group mb-6">
-                  <input type="text" className="form-control
-                    block
-                    w-full
-                    px-3
-                    py-1.5
-                    text-base
-                    font-normal
-                    text-gray-700
-                    bg-white bg-clip-padding
-                    border border-solid border-gray-300
-                    rounded
-                    transition
-                    ease-in-out
-                    m-0
-                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput123"
-                    aria-describedby="emailHelp123" placeholder="First name"/>
+                <input type="text" className="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
+                  placeholder="name"/>
                 </div>
               </div>
               <div className="form-group mb-6">
